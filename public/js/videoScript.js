13b9bbb8048db3d516436f82d4cd67be045bdac1
joordy <jmfronik@gmail.com>
2021-04-13 14:45:38 +0200

    Added functionalities back again

--- public/js/videoScript.js
+++ public/js/videoScript.js
@@ -36,6 +36,7 @@ navigator.mediaDevices
     socket.on('user-connected', (userID) => {
       newUserConnected(userID, stream)
     })
+    videoEvents()
   })
 
 peerConnection.on('call', (answerCall) => {
@@ -76,4 +77,74 @@ function addNewUserVideoStream(videoElement, stream) {
     videoElement.play()
   })
   videoGridContainer.append(videoElement)
+}
+
+function videoEvents() {
+  const muteMyself = document.querySelector('#muteMyself')
+  const stopVideo = document.querySelector('#stopVideo')
+  const endCall = document.querySelector('#endCall')
+  const inviteUser = document.querySelector('#inviteUser')
+  const openChat = document.querySelector('#openChat')
+  const copyBtn = document.querySelector('#copyRoomID')
+
+  muteMyself.addEventListener('click', (e) => {
+    let userAudioEnabled = myVideoStream.getAudioTracks()[0].enabled
+    muteMyself.classList.toggle('activeMenuItem')
+    if (!userAudioEnabled) {
+      myVideoStream.getAudioTracks()[0].enabled = true
+      muteMyself.textContent = 'Mute'
+    } else {
+      myVideoStream.getAudioTracks()[0].enabled = false
+      muteMyself.textContent = 'Unmute'
+    }
+  })
+
+  stopVideo.addEventListener('click', (e) => {
+    let userVidEnabled = myVideoStream.getVideoTracks()[0].enabled
+    console.log('Hey jou heb ik nodig', myVideoStream.getVideoTracks()[0])
+    stopVideo.classList.toggle('activeMenuItem')
+
+    if (!userVidEnabled) {
+      myVideoStream.getVideoTracks()[0].enabled = true
+      stopVideo.textContent = 'Stop Video'
+    } else {
+      myVideoStream.getVideoTracks()[0].enabled = false
+      stopVideo.textContent = 'Play Video'
+    }
+  })
+
+  endCall.addEventListener('click', (e) => {
+    console.log('C')
+    console.log(endCall)
+  })
+
+  inviteUser.addEventListener('click', (e) => {
+    const invitePopUp = document.querySelector('.invitePopUp')
+    const roomID = document.querySelector('#roomID')
+    inviteUser.classList.toggle('activeMenuItem')
+    invitePopUp.classList.toggle('activePopup')
+    roomID.value = window.location.href
+  })
+
+  openChat.addEventListener('click', (e) => {
+    const chat = document.querySelector('.chatGrid')
+    openChat.classList.toggle('activeMenuItem')
+    chat.classList.toggle('active')
+    videoContainer.classList.toggle('active')
+  })
+
+  copyBtn.addEventListener('click', (e) => {
+    const invitePopUp = document.querySelector('.invitePopUp')
+    const roomID = document.querySelector('#roomID')
+    const inputEl = document.createElement('input')
+    document.body.appendChild(inputEl)
+    inputEl.value = roomID.value
+    inputEl.select()
+    document.execCommand('copy', false)
+    inputEl.remove()
+    setTimeout(() => {
+      invitePopUp.classList.remove('activePopup')
+      inviteUser.classList.remove('activeMenuItem')
+    }, 500)
+  })
 }
