4793a4b382fa58c911ac7409c6a455305d8688fd
joordy <jmfronik@gmail.com>
2021-04-13 23:11:25 +0200

    Removed routes and made new sidepass

--- server/router/routes.js
+++ server/router/routes.js
@@ -3,19 +3,21 @@ const express = require('express')
 const router = express.Router()
 
 // Import routes
-const { home } = require('./../renders/home')
-const { notFound } = require('./../renders/notFound')
-const { dashboard } = require('./../renders/dashboard')
-const { rooms } = require('./../renders/rooms')
+const home = require('./../renders/home')
+const notFound = require('./../renders/notFound')
+// const createroom = require('../../_trash/createroom')
+const invite = require('../renders/invite.js')
+const rooms = require('./../renders/rooms')
 
 // Setting get routes
 router.get('/', home)
-router.get('/dashboard', dashboard)
 router.get('/rooms/:roomID', rooms)
+// router.get('/createroom', createroom)
 
 // router.get('*', notFound)
 
 router.post('/rooms/:roomID', rooms)
+router.post('/rooms/invite', invite)
 
 // Export router module
 module.exports = router
