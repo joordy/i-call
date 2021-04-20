c3c02a005c86853b0edb9212a1c417deb88b9ee1
joordy <jmfronik@gmail.com>
2021-04-20 11:47:04 +0200

    connection fix

--- server/index.js
+++ server/index.js
@@ -4,7 +4,6 @@ const http = require('http')
 const app = express()
 const path = require('path')
 const session = require('express-session')
-const mongoose = require('mongoose')
 const router = require('./router/routes')
 const hbs = require('./utils/hbsSetup')
 const port = process.env.PORT || 3232
@@ -26,50 +25,34 @@ const newSession = session({
   },
 })
 
-app.enable('trust proxy')
-app.engine('.hbs', hbs.engine)
-app.set('view engine', '.hbs')
-app.set('views', path.join(__dirname, './views'))
-app.use(express.static('public'))
-app.use(router)
-app.use('/peerjs', peerServer)
-app.use(
-  express.urlencoded({
-    extended: true,
-  })
-)
-app.use(express.json())
-app.use(
-  session({
-    secret: `process.env.SESSION_SECRET`,
-    cookie: { maxAge: 600000 },
-    resave: true,
-    saveUninitialized: true,
-    secure: true,
-  })
-)
-app.use((request, response, next) => {
-  if (process.env.NODE_ENV != 'development' && !request.secure) {
-    return response.redirect('https://' + request.headers.host + request.url)
-  }
-  next()
-})
-
-mongoose
-  .connect(
-    `mongodb+srv://joordy_:jjsyEnMXAbUGzJRRJ77z@icall.pjumk.mongodb.net/allUsers?retryWrites=true&w=majority`,
-    {
-      dbName: 'allUsers',
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-    }
+app
+  .enable('trust proxy')
+  .engine('.hbs', hbs.engine)
+  .set('view engine', '.hbs')
+  .set('views', path.join(__dirname, './views'))
+  .use(
+    express.urlencoded({
+      extended: true,
+    })
   )
-  .then(() => {
-    console.log('mongoDB database connected')
-  })
-  .catch((err) => {
-    console.log('Error connecting...')
+  .use(express.json())
+  .use(express.static('public'))
+  .use(
+    session({
+      secret: process.env.SESSION_SECRET,
+      cookie: { maxAge: 600000 },
+      resave: true,
+      saveUninitialized: true,
+      secure: true,
+    })
+  )
+  .use(router)
+  .use('/peerjs', peerServer)
+  .use((request, response, next) => {
+    if (process.env.NODE_ENV != 'development' && !request.secure) {
+      return response.redirect('https://' + request.headers.host + request.url)
+    }
+    next()
   })
 
 // Use Socket.io function in application
