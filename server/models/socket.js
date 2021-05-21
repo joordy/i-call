00c35b66cd9fc039e17c5a93b68fe5aefbd8938a
joordy <jmfronik@gmail.com>
2021-05-21 10:45:54 +0200

    changed data model

--- server/models/socket.js
+++ server/models/socket.js
@@ -1,10 +1,11 @@
 const sharedSessions = require('express-socket.io-session')
 const createMessage = require('./../controller/createMessage')
-const chatModel = require('./../models/data')
+const getRandomCatFacts = require('./../controller/returnCatFact')
 
 const initSocketIO = async (server, newSession) => {
   // Requires package of socket, and connect seperated server to it
   const io = require('socket.io')(server)
+  const allFacts = await dataModel()
 
   // Create socket connection
   io.on('connection', (socket) => {
@@ -29,16 +30,33 @@ const initSocketIO = async (server, newSession) => {
 
     // Sends message to client
     socket.on('message', async (obj) => {
-      // Fetch random cat fact
-      const catFact = await chatModel()
+      const num = Math.floor(Math.random() * 5) + 1
+      let fact
 
       // Send message to specific room ID, so room A doesn't receive messages from room B
       io.to(obj.room_ID).emit('createMessage', createMessage(obj))
 
       // Check if message contains the word of cat facts
+      // And send message to client-side with catfact
       if (obj.message.includes('catfact')) {
-        // Send message to client-side with catfact
-        io.to(obj.room_ID).emit('createMessage', createMessage(catFact))
+        // If num is not correct, send first one, otherwise the randomized number
+        if (!allFacts[num]) {
+          return io.to(obj.room_ID).emit(
+            'createMessage',
+            createMessage({
+              message: `${allFacts[1].text}`,
+              user: 'Cat',
+            })
+          )
+        } else {
+          return io.to(obj.room_ID).emit(
+            'createMessage',
+            createMessage({
+              message: `${allFacts[num].text}`,
+              user: 'Cat',
+            })
+          )
+        }
       }
     })
 
@@ -48,6 +66,11 @@ const initSocketIO = async (server, newSession) => {
       socket.broadcast.emit('userDisconnecting', userObj.peerID)
     })
   })
+}
+
+async function dataModel() {
+  const response = await getRandomCatFacts()
+  return response
 }
 
 // Export socket module
